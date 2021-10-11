46b6acb5d6250de8913815fcfbda7919b0c91c02
Rafael Moreira <rafaeltimoreirass@gmail.com>
2021-10-11 19:31:55 -0300

    Atual

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -1,8 +1,10 @@
-const API_KEY = "1f09c298abcad564cbce3bddb8daa7a3";
-const API_BASE = "https://api.themoviedb.org/3";
+require("dotenv").config();
+
+const varKey = process.env.API_KEY;
+const varBase = process.env.API_BASE;
 
 const basicFetch = async (endpoint) => {
-  const req = await fetch(`${API_BASE}${endpoint}`);
+  const req = await fetch(`${varBase}${endpoint}`);
   const json = await req.json();
   return json;
 };
@@ -15,56 +17,56 @@ export default {
         slug: "originals",
         title: "Originais Cappacita Movie",
         items: await basicFetch(
-          `/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`
+          `/discover/tv?with_network=213&language=pt-BR&api_key=${varKey}`
         ),
       },
       {
         slug: "trending",
         title: "Recomendados para você",
         items: await basicFetch(
-          `/trending/all/week?language=pt-BR&api_key=${API_KEY}`
+          `/trending/all/week?language=pt-BR&api_key=${varKey}`
         ),
       },
       {
         slug: "toprated",
         title: "Em alta",
         items: await basicFetch(
-          `/movie/top_rated?language=pt-BR&api_key=${API_KEY}`
+          `/movie/top_rated?language=pt-BR&api_key=${varKey}`
         ),
       },
       {
         slug: "action",
         title: "Ação",
         items: await basicFetch(
-          `/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`
+          `/discover/movie?with_genres=28&language=pt-BR&api_key=${varKey}`
         ),
       },
       {
         slug: "comedy",
         title: "Comédia",
         items: await basicFetch(
-          `/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`
+          `/discover/movie?with_genres=35&language=pt-BR&api_key=${varKey}`
         ),
       },
       {
         slug: "horror",
         title: "Terror",
         items: await basicFetch(
-          `/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`
+          `/discover/movie?with_genres=27&language=pt-BR&api_key=${varKey}`
         ),
       },
       {
         slug: "romance",
         title: "Romance",
         items: await basicFetch(
-          `/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`
+          `/discover/movie?with_genres=10749&language=pt-BR&api_key=${varKey}`
         ),
       },
       {
         slug: "documentary",
         title: "Documentários",
         items: await basicFetch(
-          `/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`
+          `/discover/movie?with_genres=99&language=pt-BR&api_key=${varKey}`
         ),
       },
     ];
@@ -76,12 +78,12 @@ export default {
       switch (type) {
         case "movie":
           info = await basicFetch(
-            `/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`
+            `/movie/${movieId}?language=pt-BR&api_key=${varKey}`
           );
           break;
         case "tv":
           info = await basicFetch(
-            `/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`
+            `/tv/${movieId}?language=pt-BR&api_key=${varKey}`
           );
           break;
         default:
